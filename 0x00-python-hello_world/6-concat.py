8b14ad15eea8b71ccb492b0b5d4f473dd2980cba
Reignel <alajembareignel@gmail.com>
2022-11-28 17:42:24 -0800

    Hot fix

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-str1 = "Holberton"
+tr1 = "Holberton"
 str2 = "School"
-str = str1 + " " + str2
-print("Welcome to {}!".format(str))
-
+str1 += ' ' + str2
+print("Welcome to {}!".format(str1))
