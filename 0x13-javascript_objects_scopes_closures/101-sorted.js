c122a920b22941474b31c9455c73bb01dc5790aa
areignel <48579497+areignel@users.noreply.github.com>
2023-05-17 22:48:23 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
+
 const dict = require('./101-data').dict;
 const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+
+Object.keys(dict).map(function (key) {
+  if (!Array.isArray(newDict[dict[key]])) {
+    newDict[dict[key]] = [];
   }
-}
+  newDict[dict[key]].push(key);
+});
+
 console.log(newDict);
